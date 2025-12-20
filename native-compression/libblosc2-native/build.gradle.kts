bf95a686d86ea24b27591aafddff5481aaad3187
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2025-12-19 17:10:09 -0700

    Create libblosc2 native jar

---
+++ native-compression/libblosc2-native/build.gradle.kts
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2025 University Corporation for Atmospheric Research/Unidata
+ * See LICENSE for license information.
+ */
+
+import java.net.URL
+import java.security.DigestInputStream
+import java.security.MessageDigest
+
+plugins {
+  id("java-base-conventions")
+  id("artifact-publishing-conventions")
+}
+
+group = "edu.ucar.unidata"
+
+var bloscVersion = "2.22.0"
+var build = "0"
+
+version = "${bloscVersion}.${build}"
+
+description = "Jar distribution of native libraries for libblosc2 compression."
+
+project.extra["project.title"] = "Native libraries for libblosc2."
+
+// zip file produced by GitHub workflow
+val libblosc2Native =
+  "libblosc2-native-${bloscVersion}-b4a818caa60bbf90bc92ec51ffd01ce3f293c8a4.zip"
+
+// sha256 checksum from GitHub workflow output
+val expectedChecksum = "895226b29a314c0beae92de66a181b66833873e8c3a8a501d0b12193bde30610"
+
+val resourceZip = file("$rootDir/project-files/native/libblosc2/$libblosc2Native")
+val fetchNativeResources =
+  tasks.register("fetchNativeResources") {
+    outputs.file(resourceZip)
+    doLast {
+      if (!resourceZip.exists()) {
+        logger.info("Fetching native libblosc2 libraries.")
+        var actualChecksum = ""
+        val resourceUrl =
+          "https://downloads.unidata.ucar.edu/netcdf-java/native/libblosc2/$libblosc2Native"
+        URL(resourceUrl).openStream().use { ips ->
+          val dips = DigestInputStream(ips, MessageDigest.getInstance("SHA-256"))
+          resourceZip.outputStream().use { ops -> dips.copyTo(ops) }
+          actualChecksum = dips.messageDigest.digest().toHexString()
+        }
+        if (actualChecksum != expectedChecksum) {
+          throw RuntimeException(
+            String.format(
+              "Error: checksum on libblosc2.zip does not match expected value.\n" +
+                "  Expected: %s\n  Actual: %s\n",
+              expectedChecksum,
+              actualChecksum,
+            )
+          )
+        }
+      }
+    }
+  }
+
+val processNativeResources =
+  tasks.register("processNativeResources", Copy::class) {
+    inputs.file(resourceZip)
+    from(zipTree(resourceZip))
+    eachFile { relativePath = RelativePath(true, *relativePath.segments.drop(1).toTypedArray()) }
+    destinationDir = layout.buildDirectory.dir("resources/main").get().asFile
+    dependsOn(fetchNativeResources)
+  }
+
+tasks.processResources { dependsOn(processNativeResources) }
+
+var publishTaskName = "nativeLibs"
+
+publishing {
+  publications {
+    create<MavenPublication>(publishTaskName) {
+      from(components["java"])
+      versionMapping {
+        usage("java-api") { fromResolutionOf("runtimeClasspath") }
+        usage("java-runtime") { fromResolutionResult() }
+      }
+    }
+  }
+}
+
+tasks
+  .matching { it.group == "publishing" }
+  .forEach { it.enabled = System.getProperty("unidata.native.publish")?.toBoolean() ?: false }
